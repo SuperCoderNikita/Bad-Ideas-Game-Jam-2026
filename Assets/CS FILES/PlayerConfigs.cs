db2ea489efa45a3eadfd176dd71adb33096b6660
SuperCoderNikita <kisiliovnikita@gmail.com>
2026-02-23 22:23:03 -0500

    added test jump mecs

--- Assets/CS FILES/PlayerConfigs.cs
+++ Assets/CS FILES/PlayerConfigs.cs
@@ -7,6 +7,7 @@ public class PlayerConfigs : MonoBehaviour
     public float speed;
     private float horizontalInput;
     private Rigidbody2D rb2d;
+    bool isOnFloor = false;
 
     void Start()
     {
@@ -16,10 +17,24 @@ public class PlayerConfigs : MonoBehaviour
     void Update()
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButton(0)) && isOnFloor == true)
+        {
+            rb2d.linearVelocity = Vector2.up * 10;
+            isOnFloor = false;
+        }
     }
 
     void FixedUpdate()
     {
         rb2d.linearVelocity = new Vector2(horizontalInput * speed, rb2d.linearVelocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            isOnFloor = true;
+        }
     }
 }
